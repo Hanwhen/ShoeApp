dd58e0865d3bee70b1e2a197df4bf086c05b46e9
Yash Patel <78617062+Ypatel02@users.noreply.github.com>
2023-05-04 15:26:20 -0400

    fixed some bugs

--- app/src/main/java/com/example/shoeapp/views/SplashActivity.java
+++ app/src/main/java/com/example/shoeapp/views/SplashActivity.java
@@ -18,18 +18,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Hide the title bar and make the splash screen full screen
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_splash);
-
-        // Start a new thread to handle the splash screen timeout
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                // Start the main activity and close the splash screen
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
